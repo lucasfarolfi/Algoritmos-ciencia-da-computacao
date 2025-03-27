07e1c35e8fa83420cf57f1e2da771f682831ea40
lucasfarolfi <lucasfarolfi.rj@gmail.com>
2025-03-27 14:03:57 -0300

    prototype - shallow copy

---
+++ arquitetura de software/design-patterns/criacionais/prototype/src/main/java/org/example/shallowCopy/User.java
@@ -0,0 +1,51 @@
+package org.example.shallowCopy;
+
+public class User implements Cloneable {
+    private String name;
+    private Integer age;
+    private Address address;
+
+    public User(String name, Integer age, Address address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address=" + address +
+                '}';
+    }
+
+    @Override
+    public User clone() throws CloneNotSupportedException {
+        return (User) super.clone();
+    }
+}
