923895f38e800ad13f3c7b17da0fae2505218aa2
lucasfarolfi <lucasfarolfi.rj@gmail.com>
2025-03-29 21:53:48 -0300

    bridge - fix locale format

--- arquitetura de software/design-patterns/estruturais/bridge/src/main/java/org/example/employeeFileConverter/converters/CSVConverter.java
+++ arquitetura de software/design-patterns/estruturais/bridge/src/main/java/org/example/employeeFileConverter/converters/CSVConverter.java
@@ -2,10 +2,12 @@ package org.example.employeeFileConverter.converters;
 
 import org.example.employeeFileConverter.employee.Employee;
 
+import java.util.Locale;
+
 public class CSVConverter implements Converter {
     @Override
     public String getEmployeeFormated(Employee employee) {
-        return String.format("%s,%d,%f",
+        return String.format(Locale.US,"%s,%d,%f",
                 employee.getName(), employee.getAge(), employee.getSalary()
         );
     }
